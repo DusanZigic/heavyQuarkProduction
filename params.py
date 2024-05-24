8372ad2dda92db150b427d1d490ac1c717ddc9b1
DusanZigic <zigic.dusan1991@gmail.com>
2024-05-24 18:18:25 +0200

    params to default

--- params.py
+++ params.py
@@ -1,10 +1,9 @@
 params = {
-    "sNN":    ["200GeV", "5020GeV"],    # collision energies
-                           # options: 200GeV, 2760GeV, 5020GeV, 5440GeV or any combination (use list of strings for multiple options)
-                           # if left empty, pT distributions will be generated for all collision energies
-    
-    "quarks": ["charm", "bottom"],      # heavy quark types
+    "quarks": "charm",      # heavy quark types
                             # options: charm, bottom or both (use list of strings for multiple options)
                             # if left empty, pT distributions will be generated for all heavy quarks
 
+    "sNN":    "5020GeV",    # collision energies
+                            # options: 200GeV, 2760GeV, 5020GeV, 5440GeV or any combination (use list of strings for multiple options)
+                            # if left empty, pT distributions will be generated for all collision energies
     }
